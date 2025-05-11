46fa0d4e582f363001b2d876cbc5c3de98152ba5
ahmed1cb2 <hijy1325@gmail.com>
2025-05-11 15:29:04 +0300

    Create Project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Routes , Route } from 'react-router-dom';
+
+function App() {
+
+  return (
+    <Routes> 
+        <Route path='/' element={<h2>Hello</h2>} />
+    </Routes>
+  )
+}
+
+export default App;
