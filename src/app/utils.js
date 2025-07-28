30b85e347bd6fdf1fa1c4879412e92a4e3ad8a8b
AHMED HASSAN <151830489+AHMED1CB@users.noreply.github.com>
2025-07-28 06:02:19 +0300

    Update utils.js

--- src/app/utils.js
+++ src/app/utils.js
@@ -1,10 +1,10 @@
 export const utils =  {
 
-    api : "https://youtube-clone-api-dnru.onrender.com/api", // No API Is On MY Git Hub Ahmed1cb/youtube-clone-api
+    api : "https://youtube-clone-api-dnru.onrender.com/api/", // No API Is On MY Git Hub Ahmed1cb/youtube-clone-api
     storage: "https://youtube-clone-api-dnru.onrender.com/api/storage/", // Change On Real Server 
     videosStorage: "https://youtube-clone-api-dnru.onrender.com/api/videos/", // Change On Real Server 
     join : (targetUri , nestedUri = '' , ...uris ) => {
-        let currentUri = utils.api + "/" + targetUri +  '/' + nestedUri; 
+        let currentUri = utils.api + targetUri +  '/' + nestedUri; 
 
         uris.map(uri => {
             currentUri += `/${uri}/`
